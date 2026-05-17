a8cf2a01fa45fc80adbcf2772d773dec066dc3eb
Vitor <vitorsantosamaral123@gmail.com>
2026-05-17 11:05:50 -0300

    Criação das entidades

---
+++ src/main/java/br/com/agendamento/model/Cliente.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.agendamento.model;
+
+/**
+ *
+ * @author vitin
+ */
+public class Cliente {
+    private String nome;
+    private String cpf;
+    
+}
